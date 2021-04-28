88548ff32ff932e3a0df450dd00baba778def059
Juliette Bénon <62658468+jbenon@users.noreply.github.com>
2021-04-28 12:33:53 +0200

    expyriment_ok

--- main_pcbs.py
+++ main_pcbs.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 from math import *
+from expyriment import control, stimuli, io, design, misc
 
 """
 Wow description
@@ -70,14 +71,44 @@ def project_structure_on_2d_plane(structure_3d, angle_x, angle_y):
     """
     Aggregates all the functions above.
     """
-    
     normal_vec = compute_plane_normal_vector(angle_x, angle_y)
     structure_proj = project_3d_structure_on_3d_plane(structure_3d, normal_vec)
     basis = create_basis_to_extract_2d_coords_from_3d_plane(structure_proj, normal_vec)
     transf_matrix = compute_transformation_matrix_from_basis(basis)
     points_2d = extract_2d_coords_from_projected_structure(structure_proj, transf_matrix)
-    return ([points_2d[:,0], points_2d[:,1]], structure_proj, normal_vec)
+    return (np.array([points_2d[:,0], points_2d[:,1]]), structure_proj, normal_vec)
+
+def return_2d_structure_for_all_angles(structure_3d, list_angle_x, list_angle_y):
+    all_points = []
+    all_structures = []
+    all_normal_vec = []
+    for i, angle_y in enumerate(list_angle_y):
+        angle_x = list_angle_x[i]
+        (points_2d, structure_proj, normal_vec) = project_structure_on_2d_plane(structure_3d, angle_x, angle_y)
+        all_points.append(points_2d)
+        all_structures.append(structure_proj)
+        all_normal_vec.append(normal_vec)
+    all_points = np.array(all_points)
+    all_structures = np.array(all_structures)
+    all_normal_vec = np.array(all_normal_vec)
+    return [all_points, all_structures, all_normal_vec]
     
+## Display with experiment
+
+def normalize_list(list, new_min, new_max):
+    # Normalize between 0 and 1
+    list = (list-np.min(list))/(np.max(list) - np.min(list))
+    # Stretch
+    list = (new_max-new_min)*list+new_min
+    return list
+
+## Find axis for exp.1
+
+def angles_exp_1(angle_step, angle_central_axis):
+    list_angles = np.arange(0, 361, angle_step)
+    list_angle_x = list_angles+angle_central_axis[0]
+    list_angle_y = list_angles+angle_central_axis[1]
+    return (list_angle_x, list_angle_y)
 
 ## test
 
@@ -102,7 +133,7 @@ plt.show()
 ## animation
 
 nb_points = 7
-nb_steps = 20
+nb_steps = 100
 dt = 0.1
 step_angle_x = 5
 step_angle_y = 0
@@ -130,30 +161,12 @@ fig = plt.figure()
 mesh = np.meshgrid(np.linspace(0,1,100), np.linspace(0,1,100))
 
 ax3d = fig.add_subplot(121, projection='3d')
+ax3d.plot(structure_3d[:, 0], structure_3d[:, 1], structure_3d[:, 2], c='k', linewidth=2)
 ax2d = fig.add_subplot(122)
 
-def return_plane_coordinates(mesh, structure_proj, normal_vec):
-    point = structure_proj[0]
-    a, b, c = normal_vec[0], normal_vec[1], normal_vec[2]
-    if a!= 0:
-        Y = mesh[0]
-        Z = mesh[1]
-        X = (np.dot(point, normal_vec) - b*Y - c*Z)/a
-    elif b!=0:
-        X = mesh[0]
-        Z = mesh[1]
-        Y = (np.dot(point, normal_vec) - a*X - c*Z)/b
-    else:
-        X = mesh[0]
-        Y = mesh[1]
-        Z = (np.dot(point, normal_vec) - a*X - b*Y)/c
-    return np.array([X, Y, Z])
 
 for i in range(nb_steps):
-    [X, Y, Z] = return_plane_coordinates(mesh, all_structures[i], all_normal_vec[i])
     if i==0:
-        ax3d.plot(structure_3d[:, 0], structure_3d[:, 1], structure_3d[:, 2], c='k', linewidth=2)
-        ax3d.plot_surface(X, Y, Z, alpha=0.5)
         line, = ax2d.plot(all_points[i][0], all_points[i][1])
         min = np.min(all_points)
         max = np.max(all_points)
@@ -163,9 +176,77 @@ for i in range(nb_steps):
         ax2d.set_yticks([])
     else:
         line.set_data(all_points[i][0], all_points[i][1])
-        ax3d.clear()
-        ax3d.plot(structure_3d[:, 0], structure_3d[:, 1], structure_3d[:, 2], c='k', linewidth=2)
-        ax3d.plot_surface(X, Y, Z, alpha=0.5)
     plt.pause(dt)
 
 plt.show()
+
+## expyriment
+
+nb_points = 10
+angle_x = 0
+list_angle_y = np.arange(0, 361, 20)
+nb_blocks = 2
+angle_step = 5
+angle_central_axis = (45,45)
+list_angle_x, list_angle_y = angles_exp_1(angle_step, angle_central_axis)
+
+exp = design.Experiment(name="Test one trial")
+control.initialize(exp)
+screen_x, screen_y = exp.screen.size
+
+for b in range(nb_blocks):
+    # One block per 3d figure
+    block = design.Block()
+    structure_3d = generate_random_3d_structure(nb_points)
+    [all_points, _, _] = return_2d_structure_for_all_angles(structure_3d, list_angle_x, list_angle_y)
+    all_x = normalize_list(all_points[:, 0, :], -screen_x/2+20, screen_x/2-20)
+    all_y = normalize_list(all_points[:, 1, :], -screen_y/2+10, screen_y/2-10)
+    
+    # One trial per view of a figure
+    for i, angle_y in enumerate(list_angle_y):
+        trial = design.Trial()
+        # Add 2d figure to the trial
+        for point in range(nb_points-1):
+            point_start = (all_x[i, point], all_y[i, point])
+            point_end = (all_x[i, point+1], all_y[i, point+1])
+            stim = stimuli.Line(point_start, point_end, line_width=4)
+            stim.preload()
+            trial.add_stimulus(stim)
+            
+        block.add_trial(trial)
+    
+    exp.add_block(block)
+
+intro = stimuli.TextLine(text=f"You will see {nb_blocks} different rotating structures. Press to start.", position = (0, 0))
+question = stimuli.TextLine(text="Did the object look rigid?", position = (0, 0))
+yes = stimuli.TextLine(text="◄ Yes", position = (-50, -40))
+no = stimuli.TextLine(text="No ►", position = (50, -40))
+
+
+# Launch experiment
+control.start()
+
+canvas = stimuli.BlankScreen()
+intro.plot(canvas)
+canvas.present()
+exp.keyboard.wait()
+
+for block in exp.blocks:
+    for trial in block.trials:
+        canvas = stimuli.BlankScreen()
+        for stim in trial.stimuli:
+            stim.plot(canvas)
+        
+        canvas.present()
+        exp.clock.wait(200)
+        
+    canvas = stimuli.BlankScreen()
+    question.plot(canvas)
+    yes.plot(canvas)
+    no.plot(canvas)
+    canvas.present()
+    key, rt = exp.keyboard.wait([misc.constants.K_LEFT,
+                                    misc.constants.K_RIGHT])
+    exp.data.add([block.id, trial.id, key, rt])
+
+control.end()
